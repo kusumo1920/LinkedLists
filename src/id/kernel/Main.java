7733af4fa039a83ba59489510a5da67201f96174
Kesit Budi Kusumo <kusumo1920@icloud.com>
2023-02-07 20:30:03 +0700

    used ListIterator

--- src/id/kernel/Main.java
+++ src/id/kernel/Main.java
@@ -1,6 +1,7 @@
 package id.kernel;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,7 +18,9 @@ public class Main {
 //        removeElements(placesToVisit);
 //        System.out.println(placesToVisit);
 
-        gettingElements(placesToVisit);
+//        gettingElements(placesToVisit);
+
+        printItinerary3(placesToVisit);
     }
 
     private static void addMoreElements(LinkedList<String> list) {
@@ -72,5 +75,43 @@ public class Main {
 
         System.out.println("Darwin is at position: " + list.indexOf("Darwin"));
         System.out.println("Melbourne is at position: " + list.lastIndexOf("Melbourne"));
+
+        // Queue retrieval method
+        System.out.println("Element from element() = " + list.element());
+
+        // Stack retrieval methods
+        System.out.println("Element from peek() = " + list.peek());
+        System.out.println("Element from peekFirst() = " + list.peekFirst());
+        System.out.println("Element from peekLast() = " + list.peekLast());
+    }
+
+    public static void printItinerary(LinkedList<String> list) {
+        System.out.println("Trip starts at " + list.getFirst());
+        for (int i = 1; i < list.size(); i++) {
+            System.out.println("--> From: " + list.get(i - 1) + " to " + list.get(i));
+        }
+        System.out.println("Trip ends at " + list.getLast());
+    }
+
+    public static void printItinerary2(LinkedList<String> list) {
+        System.out.println("Trip starts at " + list.getFirst());
+        String previousTown = list.getFirst();
+        for (String town : list) {
+            System.out.println("--> From: " + previousTown + " to " + town);
+            previousTown = town;
+        }
+        System.out.println("Trip ends at " + list.getLast());
+    }
+
+    public static void printItinerary3(LinkedList<String> list) {
+        System.out.println("Trip starts at " + list.getFirst());
+        String previousTown = list.getFirst();
+        ListIterator<String> iterator = list.listIterator(1);
+        while (iterator.hasNext()) {
+            var town = iterator.next();
+            System.out.println("--> From: " + previousTown + " to " + town);
+            previousTown = town;
+        }
+        System.out.println("Trip ends at " + list.getLast());
     }
 }
