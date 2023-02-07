6f43c65de5a565c2fd39af442e0b96682ab1dd2d
Kesit Budi Kusumo <kesit.kusumo@shopee.com>
2023-02-07 17:15:20 +0700

    First commit

---
+++ src/id/kernel/Main.java
@@ -0,0 +1,76 @@
+package id.kernel;
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+//        LinkedList<String> placesToVisit = new LinkedList<>();
+        var placesToVisit = new LinkedList<String>();
+
+        placesToVisit.add("Sydney");
+        placesToVisit.add(0, "Canberra");
+        System.out.println(placesToVisit);
+
+        addMoreElements(placesToVisit);
+        System.out.println(placesToVisit);
+
+//        removeElements(placesToVisit);
+//        System.out.println(placesToVisit);
+
+        gettingElements(placesToVisit);
+    }
+
+    private static void addMoreElements(LinkedList<String> list) {
+        list.addFirst("Darwin");
+        list.addLast("Hobart");
+        // Queue methods
+        list.offer("Melbourne");
+        list.offerFirst("Brisbane");
+        list.offerLast("Toowoomba");
+        // Stack methods
+        list.push("Alice Springs");
+    }
+
+    private static void removeElements(LinkedList<String> list) {
+        list.remove(4);
+        list.remove("Brisbane");
+
+        System.out.println(list);
+        String s1 = list.remove();
+        System.out.println(s1 + " was removed");
+
+        String s2 = list.removeFirst();
+        System.out.println(s2 + " was removed");
+
+        String s3 = list.removeLast();
+        System.out.println(s3 + " was removed");
+
+        // Queue/Deque poll methods
+        String p1 = list.poll();
+        System.out.println(p1 + " was removed");
+
+        String p2 = list.pollFirst();
+        System.out.println(p2 + " was removed");
+
+        String p3 = list.pollLast();
+        System.out.println(p3 + " was removed");
+
+        list.push("Sydney");
+        list.push("Brisbane");
+        list.push("Canberra");
+        System.out.println(list);
+
+        String p4 = list.pop();
+        System.out.println(p4 + " was removed");
+    }
+
+    private static void gettingElements(LinkedList<String> list) {
+        System.out.println("Retrieved Element = " + list.get(4));
+
+        System.out.println("First Element = " + list.getFirst());
+        System.out.println("Last Element = " + list.getLast());
+
+        System.out.println("Darwin is at position: " + list.indexOf("Darwin"));
+        System.out.println("Melbourne is at position: " + list.lastIndexOf("Melbourne"));
+    }
+}
